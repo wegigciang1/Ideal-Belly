305164160a6119cbd29e93fa92377d170835e481
dewisartika32280 <dewi.sartika@ti.ukdw.ac.id>
2019-10-26 15:59:42 +0700

    Add UserDao, Model, and ForgotPasswordController
    Modify some code

---
+++ src/main/java/rpl_ceria/ideal/belly/db/UserDAO.java
@@ -0,0 +1,70 @@
+package rpl_ceria.ideal.belly.db;
+
+import rpl_ceria.ideal.belly.model.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author dewi
+ */
+public class UserDAO {
+    
+    public static User searchUser(String email, String password) throws SQLException, ClassNotFoundException {
+        String selectStmt = "SELECT * FROM user WHERE email='" + email 
+                + "' AND password='" + password + "'";
+        try {
+            ResultSet rsUsr = DBUtil.getInstance().dbExecuteQuery(selectStmt);
+            User user = getUserFromResultSet(rsUsr);
+            return user;
+        } catch (SQLException e) {
+            System.out.println("Sedang mencari user dengan email: " + email + ", error terjadi: " + e);
+            throw e;
+        }
+    }
+
+    private static User getUserFromResultSet(ResultSet rs) throws SQLException {
+        User usr = null;
+        if (rs.next()) {
+            usr = new User();
+            usr.setId(rs.getInt("id"));
+            usr.setEmail(rs.getString("email"));
+            usr.setPassword(rs.getString("password"));
+            usr.setNama(rs.getString("nama"));
+            usr.setTanggal_lahir(rs.getDate("tanggal_lahir"));
+            usr.setTinggi_badan(rs.getDouble("tinggi_badan"));
+        }
+        return usr;
+    }
+
+    public static void updateUser(String id, User usr) throws SQLException, ClassNotFoundException {
+        String updateStmt = "UPDATE user SET";
+        updateStmt = !usr.getEmail().equals("") ? 
+                updateStmt.concat(" email='" + usr.getEmail() + "',") 
+                : updateStmt;
+        
+        updateStmt = !usr.getNama().equals("") ? 
+                updateStmt.concat(" nama='" + usr.getNama() + "',") 
+                : updateStmt;
+        
+        updateStmt = updateStmt.concat(" password='" + usr.getPassword() + "',");
+        updateStmt = updateStmt.concat(" tanggal_lahir=" + usr.getTanggal_lahir() + ",");
+        updateStmt = updateStmt.concat(" tinggi_badan=" + usr.getTinggi_badan());
+        
+        updateStmt = updateStmt.concat(" WHERE id=" + id);
+        try {
+            DBUtil.getInstance().dbExecuteUpdate(updateStmt);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+
+    public static void deleteEmpWithId(String id) throws SQLException, ClassNotFoundException {
+        String updateStmt = "DELETE FROM user WHERE id='" + id + "'";
+        try {
+            DBUtil.getInstance().dbExecuteUpdate(updateStmt);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/rpl_ceria/ideal/belly/model/User.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpl_ceria.ideal.belly.model;
+
+import java.sql.Date;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ *
+ * @author dewi
+ */
+public class User {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id.get();
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama.get();
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama.set(nama);
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email.get();
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password.get();
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+
+    /**
+     * @return the tanggal_lahir
+     */
+    public Date getTanggal_lahir() {
+        return tanggal_lahir;
+    }
+
+    /**
+     * @param tanggal_lahir the tanggal_lahir to set
+     */
+    public void setTanggal_lahir(Date tanggal_lahir) {
+        this.tanggal_lahir = tanggal_lahir;
+    }
+
+    /**
+     * @return the tinggi_badan
+     */
+    public double getTinggi_badan() {
+        return tinggi_badan.get();
+    }
+
+    /**
+     * @param tinggi_badan the tinggi_badan to set
+     */
+    public void setTinggi_badan(double tinggi_badan) {
+        this.tinggi_badan.set(tinggi_badan);
+    }
+    private SimpleIntegerProperty id = new SimpleIntegerProperty();
+    private SimpleStringProperty nama = new SimpleStringProperty();
+    private SimpleStringProperty email = new SimpleStringProperty();
+    private SimpleStringProperty password = new SimpleStringProperty();
+    private Date tanggal_lahir;
+    private SimpleDoubleProperty tinggi_badan = new SimpleDoubleProperty();
+}
